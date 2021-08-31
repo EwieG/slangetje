8e702d89c210f90439b5273d2ec5cc88b3b2e514
axtck <alexandertack@hotmail.be>
2021-08-31 20:06:15 +0200

    Add slangetje class and main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from slangetje import Slangetje
+
+if __name__ == "__main__":
+    slang = Slangetje("Snake", 12)
+    slang.introduce()
--- slangetje.py
+++ slangetje.py
@@ -1,1 +1,18 @@
-print("slangetje")
+class Slangetje():
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+        self.alive = True
+
+    def introduce(self):
+        print(f"Snake name: {self.name}\nSnake size: {self.size}")
+        return
+
+    def grow(self):
+        self.size += 1
+
+    def shrink(self):
+        self.size -= 1
+
+    def die(self):
+        self.alive = False
